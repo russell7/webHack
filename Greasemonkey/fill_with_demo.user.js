e6cccf42a880d67f7339812bd60d572d545861b2
洪运 <russel7@gmail.com>
2018-09-05 14:44:11 +0800

    enhance auto fill scenario
    add case for textarea
    add branch for email and date

--- Greasemonkey/fill_with_demo.user.js
+++ Greasemonkey/fill_with_demo.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         dev input
 // @namespace    http://holer.org/
-// @version      0.1
+// @version      0.2
 // @description  try to fill the form with values
 // @author       Russell
 // @include     /^https?://192.168.1.\d{1,3}.*$/
-// @include     /^https?://localhost.*$/
+// @include     /^https?://.*.newstar.org.cn.*$/
+// @include     /^https?://.*.geestu.group.*$/
 // @grant        none
 // ==/UserScript==
 
@@ -30,7 +31,7 @@ function main() {
     };
 
     window.decorateVal = function (oriVal){
-        return oriVal + getRandomArbitrary(100, 999);
+        return oriVal + getRandomArbitrary(11, 999);
     };
 
     window.fillDemo = function () {
@@ -49,11 +50,33 @@ function main() {
                     _this.val(_this.val() % Math.pow(10, _this.attr("maxlength")));
                 }
             } else if ("email" == _type) {
-                _this.val(getRandomString(7) + "@" + getRandomString(3) + ".dm" );
+                if(getRandomArbitrary(1,9)>5){
+                    _this.val(getRandomString(7) + "@" + getRandomString(3) + ".dm" );
+                } else{
+                    _this.val("test@zzxes.com.cn");
+                }
             }
             if (_this.attr("name") && _this.attr("name").match(/mobilePhone/i)) {
                 _this.val(getRandomArbitrary(13300000000, 18999999999));
             }
+            if (_this.attr("name") && _this.attr("name").match(/.*[Dd]ate.*/i)) {
+                var dateValue = new Date();
+                dateValue.setDate(dateValue.getDate() + getRandomArbitrary(-7,7));
+                var year = dateValue.getFullYear();
+                var month = dateValue.getMonth() + 1;
+                month = month < 10 ? "0" + month : month;
+                var day = dateValue.getDate();
+                day = day < 10 ? "0" + day : day;
+                var dateStr = year + "-" + month + "-" + day;
+                _this.val(dateStr);
+            }
+        });
+        $("textarea").each(function () {
+            var _this = $(this);
+            if (!_this.is(":visible") || _this.val()) {
+                return;
+            }
+        _this.val(decorateVal($("label[for=" + $(this).attr("id") + "]").text().replace(" ", "") + "demo"));
         });
     };
 
